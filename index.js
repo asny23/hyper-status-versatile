3f277e7d561e308cfaf061332e499570d017b65c
asny23 <asny23+github@gmail.com>
2023-02-16 23:57:07 +0900

    implement getting current dir

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const { exec } = require('child_process')
+
+let pid
+
+const setCwd = (pid) => {
+  exec(`lsof -p ${pid} | awk '$4=="cwd"' | tr -s ' ' | cut -d ' ' -f9-`, (_err, stdout) => {
+    console.log(`cwd=${stdout.trim()}`)
+  })
+}
+
+exports.middleware = (store) => (next) => (action) => {
+  const uids = store.getState().sessions.sessions
+  if (action.type === 'SESSION_SET_XTERM_TITLE') {
+    pid = uids[action.uid].pid
+  }
+  if (action.type === 'SESSION_USER_DATA') {
+    const { data } = action
+    if (data.indexOf("\r") >= 0) {
+      setCwd(pid)
+    }
+  }
+  next(action)
+}
+
 exports.decorateConfig = (config) => {
   const colorFG = config.foregroundColor || '#fff'
   return Object.assign({}, config, {
